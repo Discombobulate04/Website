1e8ead041d9b84455e40c6cb75bbe9869bc11f30
Discombobulate04 <RobinKutlu@hotmail.com>
2026-04-22 09:12:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
 const app = express();
 
-app.use((req, res, next) => {
-  const auth = { login: 'principessa', password: 'bubulovesyou' };
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+app.use(session({
+  secret: 'supersecretkey',
+  resave: false,
+  saveUninitialized: true
+}));
 
-  if (login === auth.login && password === auth.password) {
+// Hardcoded user (you can upgrade later)
+const USER = {
+  username: 'principessa',
+  password: 'bubulovesyou'
+};
+
+// Auth middleware
+function requireLogin(req, res, next) {
+  if (req.session.user) {
     return next();
   }
+  res.redirect('/login');
+}
 
-  res.set('WWW-Authenticate', 'Basic realm="Protected"');
-  res.status(401).send('Login required');
+// Routes
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>You're Logged In Baby</h1>
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-    <a href="https://www.mediafire.com/file/88bdbluweoug4tn/Intro_to_the_World_of_Data_Science_and_AI.mp4/file" target="_blank">
-      Intro to the World of Data Science and AI
-    </a><br><br>
+  if (username === USER.username && password === USER.password) {
+    req.session.user = username;
+    return res.redirect('/');
+  }
 
-    <a href="https://www.mediafire.com/file/r0podthzia5fnbf/Introduction_to_Python.mp4/file" target="_blank">
-      Introduction to Python
-    </a><br><br>
+  res.send('Wrong login');
+});
 
-    <a href="https://www.mediafire.com/file/x42jm7j6agulej6/Data_Science_and_Artificial_Intelligence_Application_Case_Study.mp4/file" target="_blank">
-      Data Science and Artificial Intelligence
-    </a><br><br>
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
 
-    <a href="https://www.mediafire.com/file/86wwfff4hidl07a/Mathematics_and_Statistics_Behind_DS.mp4/file" target="_blank">
-      Mathematics and Statistics Behind DS
-    </a>
-  `);
+app.get('/', requireLogin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
